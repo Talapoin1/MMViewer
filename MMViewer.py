0e6cf57849133f4f97fac13aeaafcc96ae24d7fc
Talapoin1 <jonathanresch@gmail.com>
2021-04-07 18:56:55 -0500

    Update MMViewer.py
    switching from os to using a raw string. fingers crossed

--- MMViewer.py
+++ MMViewer.py
@@ -5,9 +5,7 @@ from geopy.geocoders import GoogleV3 #cannot store goole API data longer then 30
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
 
-import os
-
-fixsoidonthavetolearnsomethingnew = os.path.normpath('C:\\Users\Talapoin\Desktop\Data_Science\MMdata\generateReqs\APIKey.txt')
+fixsoidonthavetolearnsomethingnew = r'C:\\Users\Talapoin\Desktop\Data_Science\MMdata\generateReqs\APIKey.txt'
 with open(fixsoidonthavetolearnsomethingnew) as f:
     lines = f.readlines()
 key=lines[0]
