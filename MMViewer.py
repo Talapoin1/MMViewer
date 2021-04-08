fa7512371496cc9bd27130e010867a6b5939f511
Talapoin1 <jonathanresch@gmail.com>
2021-04-07 19:00:44 -0500

    Update MMViewer.py
    trying os again with forward slashes this time

--- MMViewer.py
+++ MMViewer.py
@@ -5,7 +5,9 @@ from geopy.geocoders import GoogleV3 #cannot store goole API data longer then 30
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
 
-fixsoidonthavetolearnsomethingnew = r'C:\\Users\Talapoin\Desktop\Data_Science\MMdata\generateReqs\APIKey.txt'
+import os
+
+fixsoidonthavetolearnsomethingnew = os.path.normpath('C:/Users/Talapoin/Desktop/Data_Science/MMdata/generateReqs/APIKey.txt'
 with open(fixsoidonthavetolearnsomethingnew) as f:
     lines = f.readlines()
 key=lines[0]
